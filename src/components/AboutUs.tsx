12125526d702626852a3d4c2ef24ae6c2e0197d7
Ankush307 <ankushdhull966@gmail.com>
2025-02-04 09:34:48 +0530

    change color

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -3,22 +3,20 @@ import Image from "next/image";
 import { ABOUT_US_LIST } from "@/utils/helper";
 const AboutUs = () => {
     return (
-        <div className="bg-[#0A0A2B] sm:pt-[84px] pt-0 pb-16 relative">
+        <div className="bg-[#0A0A2B] lg:pt-[140px] sm:pt-16 pt-7 pb-16 relative">
             <div className="max-w-[1141px] mx-auto">
                 <div className="flex items-center max-lg:flex-wrap gap-16 max-lg:gap-10 px-4">
                     <div className="w-6/12 max-lg:w-full">
                         <div className="relative">
-                            <div className="relative lg:mb-7 sm:mb-5 mb-[35px]">
+                            <div className="relative">
                                 <h1 className="pt-8 xl:text-5xl lg;text-4xl text-3xl font-black text-white tracking-[6px] relative z-[1] max-md:pt-10">About Us</h1>
                                 <span className="absolute inset-0 xl:text-5xl lg;text-4xl text-3xl font-black bg-gradient-to-t to-light-purple via-light-pink from-light-orange bg-clip-text text-transparent tracking-[6px] md:top-[31px] top-10 -translate-x-0.5">About Us</span>
                             </div>
-                            <div className="sm:pt-[25px]">
+                            <div className="lg:pt-7 md:pt-5 pt-[35px] flex flex-col lg:gap-2 gap-3">
                                 {ABOUT_US_LIST.map((obj, i) => (
-                                    <div key={i}>
-                                        <div className="flex items-center gap-5 max-xl:my-4">
-                                            <Image src="/assets/images/webp/about-us-img.webp" width={29} height={29} alt="icons" className="w-full max-w-[39px]" />
-                                            <p className="text-white font-normal text-base leading-6 max-w-[510px] max-xl:max-w-none">{obj.description}</p>
-                                        </div>
+                                    <div key={i} className="flex items-center gap-5">
+                                        <div className="">{obj.icon}</div>
+                                        <p className="text-white font-normal text-base leading-6 max-w-[510px] max-xl:max-w-none">{obj.description}</p>
                                     </div>
                                 ))}
                             </div>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -22,27 +22,10 @@ const Header = () => {
         document.body.classList.remove("overflow-hidden");
       }
     };
-
-    const handleScroll = () => {
-      if (window.scrollY > 300) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    handleOverflow();
-    window.addEventListener("resize", handleOverflow);
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("resize", handleOverflow);
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, [open]);
 
   return (
-    <div className={`px-4 !mx-auto sm:py-5 py-1 w-full fixed top-0 z-10 transition-all duration-300 ${scrolled ? "bg-black" : "bg-black/50"}`}>
+    <div className={`px-4 !mx-auto sm:py-5 py-1 w-full fixed top-0 z-10 transition-all duration-300 bg-black/50`}>
       <div className="container mx-auto flex items-center justify-between max-w-[1140px] ">
         <a href="/">
           <img src="/assets/images/png/page-logo.png" alt="page-logo" className="pointer-events-none size-[68px]" />
@@ -50,14 +33,14 @@ const Header = () => {
         <div className={`flex gap-[55px] max-xl:gap-10 max-lg:fixed max-lg:top-0 max-lg:-right-full max-lg:h-full max-lg:w-full max-lg:justify-center max-lg:items-center transition-all duration-300 max-lg:flex-col max-lg:bg-black max-lg:z-10 ${open ? "max-lg:!right-0" : ""}`}>
           <div className="flex max-lg:flex-col gap-5 items-center">
             {HEADER_LIST.map((item, i) => (
-              <a key={i} onClick={closeHandler} href={item.link} className="text-white !whitespace-nowrap font-normal text-base leading-[19.2px] hover:text-blue-400 transition-all duration-300">{item.title}</a>
+              <a key={i} onClick={closeHandler} href={item.link} className="text-white !whitespace-nowrap font-normal text-base leading-[19.2px] hover:text-light-pink transition-all duration-300 max-lg:text-xl ">{item.title}</a>
             ))}
           </div>
           <button onClick={closeHandler} className="text-white hidden max-md:block p-[11px_23px_11px_25px] bg-gradient-to-t to-light-purple via-light-pink from-light-orange text-xl font-extrabold leading-6 tracking-[6%] rounded-lg transition-all duration-300">Mint Now</button>
           <button onClick={closeHandler} className="text-white max-md:hidden p-[11px_23px_11px_25px] bg-gradient-to-t to-light-purple via-light-pink from-light-orange text-xl font-extrabold leading-6 tracking-[6%] rounded-lg transition-all duration-300"> Mint Now</button>
         </div>
         <div className="flex items-center justify-center gap-10 lg:hidden">
-          <button onClick={closeHandler} className="text-white max-md:hidden lg:hidden p-[11px_23px_11px_25px] bg-gradient-to-t to-light-purple via-light-pink from-light-orange text-xl font-extrabold leading-6 tracking-[6%] rounded-lg transition-all duration-300"> Mint Now</button>
+          <button className="text-white max-md:hidden lg:hidden p-[11px_23px_11px_25px] bg-gradient-to-t to-light-purple via-light-pink from-light-orange text-xl font-extrabold leading-6 tracking-[6%] rounded-lg transition-all duration-300"> Mint Now</button>
           <button onClick={handleClick} className="flex flex-col items-center justify-between relative z-20 overflow-hidden sm:h-8 sm:w-[39px] h-[19px] w-5 lg:hidden">
             <span className={`w-full h-[3px] sm:h-[5px] transition-all duration-300 bg-white rounded-lg ${open ? " translate-x-10" : ""}`}></span>
             <span className={`w-full h-[3px] sm:h-[5px] transition-all duration-300 bg-white rounded-lg ${open ? " -translate-x-10" : ""}`}></span>
